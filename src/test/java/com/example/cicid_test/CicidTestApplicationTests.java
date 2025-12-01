5e20d533dd6f260c7a3cd41b3c5898b494c598eb
Hellicat <jhccop@naver.com>
2025-12-01 09:27:06 +0900

    push

---
+++ src/test/java/com/example/cicid_test/CicidTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicid_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicidTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
